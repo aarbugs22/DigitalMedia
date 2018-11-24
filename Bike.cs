9faaae9dbe215a076ffb3d78bb2cefd427fc631b
aarbugs22 <44698720+aarbugs22@users.noreply.github.com>
2018-11-24 15:39:20 +0000

    Add files via upload

---
+++ Bike.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bike : MonoBehaviour {
+
+    public GameObject bikeItem;
+
+	void Update ()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            GameObject.FindGameObjectsWithTag("Player");
+            transform.Translate(5f, 0f, 0f);
+            //transform.position = FindGameObject("bikeItem");
+        }
+	}
+}       
